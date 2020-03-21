9716855e4e1d544267b7b524d723e8c6e91ca394
soyspinoza <legalup@yahoo.com>
2020-03-21 14:32:33 -0400

    cleaned up template a bit

--- cf/codeforces.cpp
+++ cf/codeforces.cpp
@@ -1,87 +1,20 @@
-#ifndef _GLIBCXX_NO_ASSERT
-#include <cassert>
-#endif
-#include <cctype>
-#include <cerrno>
-#include <cfloat>
-#include <ciso646>
-#include <climits>
-#include <clocale>
-#include <cmath>
-#include <csetjmp>
-#include <csignal>
-#include <cstdarg>
-#include <cstddef>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <ctime>
 
-#if __cplusplus >= 201103L
-#include <ccomplex>
-#include <cfenv>
-#include <cinttypes>
-#include <cstdbool>
-#include <cstdint>
-#include <ctgmath>
-#include <cwchar>
-#include <cwctype>
-#endif
 
-#include <algorithm>
-#include <bitset>
-#include <complex>
-#include <deque>
-#include <exception>
-#include <fstream>
-#include <functional>
-#include <iomanip>
-#include <ios>
-#include <iosfwd>
 #include <iostream>
-#include <istream>
+#include <fstream>
+#include <sstream>
 #include <iterator>
 #include <limits>
-#include <list>
-#include <locale>
-#include <map>
-#include <memory>
-#include <new>
-#include <numeric>
-#include <ostream>
 #include <queue>
-#include <set>
-#include <sstream>
-#include <stack>
-#include <stdexcept>
-#include <streambuf>
-#include <string>
-#include <typeinfo>
-#include <utility>
-#include <valarray>
 #include <vector>
-
-#if __cplusplus >= 201103L
-#include <array>
-#include <atomic>
-#include <chrono>
-#include <condition_variable>
-#include <forward_list>
-#include <future>
-#include <initializer_list>
-#include <mutex>
-#include <random>
-#include <ratio>
-#include <regex>
-#include <scoped_allocator>
-#include <system_error>
-#include <thread>
 #include <tuple>
-#include <typeindex>
-#include <type_traits>
 #include <unordered_map>
-#include <unordered_set>
-#endif
+#include <set>
+#include <string>
+#include <algorithm>
+#include <bitset>
+#include <numeric>
+
 
 using namespace std;
 
@@ -98,10 +31,12 @@ using namespace std;
 
 #define MAXTOP 10000000 //this is the biggest topcoder can do
 
-typedef deque<int> di;
-typedef vector<int> vi;
-typedef set<int> si;
-typedef unsigned long long ull;
+using di=deque<int>;
+using vi=vector<int>;
+using si=set<int>;
+using ull=unsigned long long;
+using pii=pair<int,int>;
+
 
 template <class T>
 void printv(vector<T> v)
@@ -109,14 +44,72 @@ void printv(vector<T> v)
   copy(a2z(v),ostream_iterator<T>(cout,","));
 }
 
-//#define MAXN 100
-//int dp[MAXN][MAXN][MAXN][MAXN];
-//memset(dp, -1, sizeof(dp));
+#define MAXN 50
 
-int
-main(int argc, char**argv)
-{
 
-  ios_base::sync_with_stdio(false); cout.setf(ios::fixed); cout.precision(20);
+
+
+class  ABCPath{
+  int dp[MAXN][MAXN];
+  vector<string> mygrid;
   
-}
+  int find_longest(int row, int col)
+  {
+    if(dp[row][col] >=0) return dp[row][col];
+    char ch = mygrid[row][col];
+    vector<pii> nbrs;
+    int numrows = mygrid.size();
+    int numcols = mygrid[0].size();
+    for(int r=row-1; r<=row+1; r++)
+      for(int c=col-1; c<=col+1; c++){
+        if(r==row && c==col) continue;
+        if(c>=0 && c<numcols && r>=0 && r<numrows){
+          char nxtch = mygrid[r][c];
+          if(nxtch-ch == 1) nbrs.push_back(make_pair(r,c));
+        }
+      }
+
+    priority_queue<int> lengths;
+    for(auto pr : nbrs){
+      lengths.push(find_longest(pr.first,pr.second));
+    };
+    
+    int longest(0);
+    if(!lengths.empty()) longest = lengths.top()+1;
+    dp[row][col] = longest;
+    return longest;
+  };
+  
+ public:
+  
+  int length(vector <string> grid)
+  {
+    memset(dp, -1, sizeof(dp));
+    mygrid = grid;
+    priority_queue<int> pq;
+
+    int nrs = grid.size();
+    int ncs = grid[0].size();
+    for(int r=0; r<nrs; r++)
+      for(int c=0; c<ncs; c++)
+        if(grid[r][c]=='A') pq.push(find_longest(r,c));
+
+    if(pq.empty()) return 0;
+    return pq.top()+1;
+  }
+};
+
+
+// int
+// main(int argc, char**argv)
+// {
+
+//   ios_base::sync_with_stdio(false); cout.setf(ios::fixed); cout.precision(20);
+//   string str = "abc";
+
+//   int sz = str.size();
+//   cout << "the last char of str is:" << str[sz-1] << endl;
+    
+  
+// }
+ 
--- cf/tester.py
+++ cf/tester.py
@@ -11,7 +11,7 @@ import sys
 
 if __name__ == "__main__":
     
-    proc = subprocess.Popen(["/home/luis/workspace/goodproblems_probs/cf/cf"], shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    proc = subprocess.Popen(["/home/luis/workspace/tc_greed/cf/cf"], shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     fh = open(sys.argv[1], "r") 
     inputstr=""
     for line in fh: 
