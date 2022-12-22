a97882c31efff22e872019cf6cfd8169ddddc757
legalup <luis@bellmanheld>
2022-12-22 09:01:20 -0500

    init checking

---
+++ advent_of_code_2022/aoc.cpp
@@ -0,0 +1,104 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+#define fi(a, b) for(int i=((int)(a)); i < ((int)(b)); i++)
+#define fj(a, b) for(int j=((int)(a)); j < ((int)(b)); j++)
+#define fk(a, b) for(int k=((int)(a)); k < ((int)(b)); k++)
+#define fori(a) for(int i=((int)(0)); i < ((int)(a.size())); i++)
+#define forj(a) for(int j=((int)(0)); j < ((int)(a.size())); j++)
+#define a2z(v)            (v).begin(),(v).end()
+#define isin(b,a)        find(all(a),(b))!=(a).end()
+#define isnotin(b,a)        find(all(a),(b))==(a).end()
+#define len(v)   (v).size()
+
+
+using vi=vector<int>;
+using si=set<int>;
+using vd=vector<double>;
+using graph = map<int,multiset<int>>;
+
+using ull=long long;
+using pii = pair<ull,ull>;
+using tri=tuple<int,int,int>;
+using pq = priority_queue<int, vector<int>, less<int>>;
+
+
+template <typename T, class Container = vector<T>,
+        class Compare = less<typename Container::value_type>>
+class priority_queue_with_remove : public priority_queue<T, Container, Compare> {
+public:
+    bool remove(const T &valueToRemove) {
+        auto itr = std::find(this->c.begin(), this->c.end(), valueToRemove);
+        if (itr == this->c.end()) {
+            return false;
+        }
+        this->c.erase(itr);
+        // ideally we should not be rebuilding the heap as we are removing only one element
+        // a custom implementation of priority queue can adjust only one element in O(logN)
+        std::make_heap(this->c.begin(), this->c.end(), this->comp);
+        return true;
+    }
+};
+
+struct gtpii
+{
+    bool operator()(const pii & p1, const pii & p2)
+    {
+        auto & [a1,idx1] = p1;
+        auto & [a2,idx2] = p2;
+        if(a1 != a2) return a1>a2;
+        return idx1 > idx2;
+    }
+};
+
+using pqpii = priority_queue<pii, vector<pii>, gtpii>;
+
+void
+split( vector<string> & theStringVector,  /* Altered/returned value */
+       const  string  & theString,
+       const  string & theDelimiters)
+{
+  assert(!theDelimiters.empty());
+  char buffy[theString.size()];
+  strcpy(buffy,theString.c_str());
+
+  char  * str1 = strtok(buffy,theDelimiters.c_str());
+  theStringVector.push_back(str1);
+
+  while((str1 = strtok(0,theDelimiters.c_str()))) theStringVector.push_back(str1);
+  return;
+
+}
+
+template<typename T>
+void run(T & cc){
+
+  vector<string> lines;
+  for (std::string line; std::getline(cc, line); ) 
+  {
+    lines.push_back(line);
+  }
+      
+
+}
+int
+main(int argc, char**argv)
+{
+
+  ios_base::sync_with_stdio(false); cout.setf(ios::fixed); cout.precision(20);
+   
+ //#define TEST
+
+#ifdef TEST
+  ifstream myfile ("data.txt");
+  run(myfile);
+
+  
+#else
+  run(cin);
+#endif
+  
+}
+ 
