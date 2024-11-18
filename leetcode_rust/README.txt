790aeb54421c3e157271400c966eafc052a9eeae
luis <legalup@proton.me>
2024-11-18 07:07:26 -0500

    working

---
+++ leetcode_rust/README.txt
@@ -0,0 +1,3 @@
+to get rustc and cargo and all that, just type
+nix-shell
+here and you are good to go.
--- leetcode_rust/shell.nix
+++ leetcode_rust/shell.nix
@@ -1,24 +1,20 @@
-
-{ src ? builtins.fetchGit {
-    url = "git@ghe.anduril.dev:anduril/anduril-nixpkgs.git";
-    ref = "tff/add-hop";
-    rev = "7b215caa5c6f569cc7edce2f7c654dc1637c943d";
-  }
-, cross ? false
-, cross-system ? "aarch64-multiplatform"
-, local ? false
-}:
 let
-  pkgs-native = import (if local then <nixpkgs> else src) {};
-  pkgs = if cross then pkgs-native.pkgsCross."${cross-system}" else pkgs-native;
+  rust-overlay = builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz";
+  pkgs = import <nixpkgs> {
+    overlays = [(import rust-overlay)];
+  };
+  toolchain = pkgs.rust-bin.fromRustupToolchainFile /home/luis/toolchain.toml;
 in
-with pkgs;
-mkShell {
-  inputsFrom = with anduril; [
-    (rust.addSrcToCrateToolchain rust.hop)
-  ];
-  nativeBuildInputs = [
-    anduril.rust.makeCLionToolchain
-    cmake
-  ];
-}
+  pkgs.mkShell {
+    packages = [
+      toolchain
+    ];
+  }
+# shell.nix
+
+  
+  
+  
+
+  
+  
