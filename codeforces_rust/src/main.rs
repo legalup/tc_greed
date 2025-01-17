894da7cb75fb716d3f12cd31a9e3f9896194ec08
luis galup <legalup@pm.me>
2025-01-17 05:43:53 -0500

    latest

--- codeforces_rust/src/main.rs
+++ codeforces_rust/src/main.rs
@@ -35,13 +35,64 @@ impl Scanner {
     }
 }
 
-pub fn doit( nums : &mut Vec::<(i32,i32)>) {
-    println!("here is nums {:?}",nums);
+struct Solution {
+    cache : HashMap<(usize,usize),bool>,
+    n : usize,
+}
+
+impl Solution{
+
+    fn new(n : usize) -> Solution {
+        Solution {
+            cache : HashMap::<(usize,usize),bool>::new(),
+            n,
+        }
+    }
+    // return true ifplayer1 can win, false if cannot
+    pub fn player1_turn(&self,apos : usize, bpos : usize)->bool{
+        if apos+2usize == bpos {
+            return true;
+        }
+        else if apos+1 == bpos {
+            return false;
+        }
+        
+        return !self.player2_turn(apos+1,bpos);
+    } 
+
+    //return true is player2 can win from pos, false is canot
+    pub fn player2_turn(&self, apos: usize, bpos : usize) ->bool {
+        if apos+2usize==bpos {
+            return true;
+        }
+        else if bpos == apos+1usize {
+            return false;
+        }
+        return !self.player1_turn(apos,bpos-1);
+
+    }
+}
+
+pub fn doit( grid : &mut Vec::<Vec::<i32>>, path : &str){
+    let mut currr = 0usize;
+    let mut currc = 0usize;
+
+    let cvec: Vec<char> = path.chars().collect();
+    
+    let mut path  = path.chars().map(|ch| {
+        if ch == 'D' {
+            currr+=1;
+        }
+        else {
+            currc+=1;
+        }
+        (currr, currc)
+    }).collect::<Vec::<(usize,usize)>>();
 }
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
-    dbg!(&args);
+    //dbg!(&args);
 
     let mut scan = Scanner::default();
     let out = &mut BufWriter::new(stdout());
@@ -49,14 +100,23 @@ fn main() -> io::Result<()> {
     let t = scan.next::<usize>();
     for _ in (0..t){
         let n = scan.next::<usize>();
-        let mut intervals : Vec<(i32,i32)> = (0..n).map(|_| {let l = scan.next(); let r = scan.next(); (l,r)}).collect();
-        doit(&mut intervals);
+        let m = scan.next::<usize>();
+        let path : String = scan.next::<String>();
+        let mut grid = vec![vec![0 ; m];n];
+        for r in (0..n){
+            for c in (0..m) {
+                grid[r][c] = scan.next::<i32>();
+            }
+        }
+       
+        doit(&mut grid);
+        
     }
 
         Ok(())
     }
 
-   
-    
+
+
     
   
